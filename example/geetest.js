37df41057b9cce4a6eac5f6b7100e03e0a62b237
imagetyperz-api <imagetyperz@gmail.com>
2021-07-27 20:11:46 +0300

    added api_server parameter for geetest

--- example/geetest.js
+++ example/geetest.js
@@ -2,6 +2,7 @@ var geetest_params = {};
 geetest_params.domain = 'https://your-site.com';
 geetest_params.challenge = 'eea8d7d1bd1a933d72a9eda8af6d15d3';
 geetest_params.gt = '1a761081b1114c388092c8e2fd7f58bc';
+//geetest_params.api_server = 'api.geetest.com'     // geetest domain - optional
 //geetest_params.proxy = '126.45.34.53:123';     // optional
 //geetest_params.user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0';   // optional
 
